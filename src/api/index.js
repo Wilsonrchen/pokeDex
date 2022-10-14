09810906e878998ed718b8edb79574e1a867f3ce
root <root@Wilson-Desktop.localdomain>
2022-10-14 13:03:49 -0700

    changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,10 @@
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=10'
+const url = "https://pokeapi.co/api/v2/pokemon?limit=10";
 
+export const fetchPokemon = async () => {
+  const response = await fetch({ url });
+  const result = await response.json();
+  return result;
+};
 /* 
     Write a AJAX helper function which takes a url, and returns a list of pokemon 
         TIPS: use the url above in the browser to see what we're getting from the API
